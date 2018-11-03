68a4a973aa1ff59fecae1cb1f3496ad715463f0a
Isabella Lau <izzielau@airbears2-10-142-178-140.airbears2.1918.berkeley.edu>
2018-11-03 04:17:54 -0700

    Added Chameleon colors.

--- PoopityScoop/Constants.swift
+++ PoopityScoop/Constants.swift
@@ -13,5 +13,12 @@ class Constants {
     static let BLUE = UIColor(red: 117.0/255.0, green: 179.0/255.0, blue: 255.0/255.0, alpha: 1.0)
     static let DARK_BLUE = UIColor(red: 109.0/255.0, green: 158.0/255.0, blue: 235.0/255.0, alpha: 1.0)
     static let DARKER_BLUE = UIColor(red: 109.0/255.0, green: 158.0/255.0, blue: 255.0/255.0, alpha: 1.0)
+    
     static let DARK_GRAY = UIColor(red: 30.0/255.0, green: 30.0/255.0, blue: 30.0/255.0, alpha: 1.0)
+    
+    static let LIGHT_MINT = UIColor(red: 59.0/255.0, green: 194.0/255.0, blue: 153.0/255.0, alpha: 1.0)
+    static let DARK_MINT = UIColor(red: 31.0/255.0, green: 140.0/255.0, blue: 115.0/255.0, alpha: 1.0)
+    
+    static let LIGHT_CORAL = UIColor(red: 247.0/255.0, green: 118.0/255.0, blue: 127.0/255.0, alpha: 1.0)
+    static let DARK_CORAL = UIColor(red: 184.0/255.0, green: 74.0/255.0, blue: 89.0/255.0, alpha: 1.0)
 }
